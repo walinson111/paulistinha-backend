4efe108dfaf624504810ce70f9aa614ebb6a0b39
Nicolas Augusto <nick.nogueira02@gmail.com>
2025-12-09 23:56:56 -0300

    :sparkles: feat: implemented cors and corsConfigSource for security config

--- paulistinha/src/main/java/com/mercado/paulistinha/config/SecurityConfig.java
+++ paulistinha/src/main/java/com/mercado/paulistinha/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.mercado.paulistinha.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -8,6 +10,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration; 
+import org.springframework.web.cors.CorsConfigurationSource; 
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.mercado.paulistinha.service.FuncionarioUserDetailsService;
 
@@ -22,8 +27,13 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+            
             .csrf(csrf -> csrf.disable())
+            
             .authorizeHttpRequests(auth -> auth
+
+                .requestMatchers("/auth/login").permitAll()
                 .requestMatchers("/funcionarios/**").hasRole("GERENTE")
                 .anyRequest().authenticated()
             )
@@ -37,6 +47,24 @@ public class SecurityConfig {
             .userDetailsService(userDetailsService);
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        
+        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        
+        configuration.setAllowedHeaders(List.of("*"));
+        
+
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean
