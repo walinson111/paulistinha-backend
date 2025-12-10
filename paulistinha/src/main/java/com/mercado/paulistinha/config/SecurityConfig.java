d4b01447540ac66046b7924e36d728e58e212182
Walinson Pereira <walinsonoliveirapereira@gmail.com>
2025-12-10 18:32:49 -0300

    🔨 refact: Funcao de deletar produtos

--- paulistinha/src/main/java/com/mercado/paulistinha/config/SecurityConfig.java
+++ paulistinha/src/main/java/com/mercado/paulistinha/config/SecurityConfig.java
@@ -4,13 +4,14 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration; 
+import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource; 
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
@@ -34,6 +35,8 @@ public class SecurityConfig {
             .authorizeHttpRequests(auth -> auth
 
                 .requestMatchers("/auth/login").permitAll()
+                .requestMatchers(HttpMethod.GET, "/produtos").permitAll()
+                .requestMatchers(HttpMethod.DELETE, "/produtos/**").hasAnyRole("GERENTE", "ESTOQUISTA")
                 .requestMatchers("/funcionarios/**").hasRole("GERENTE")
                 .anyRequest().authenticated()
             )
--- paulistinha/src/main/java/com/mercado/paulistinha/controller/ProdutoController.java
+++ paulistinha/src/main/java/com/mercado/paulistinha/controller/ProdutoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,7 +21,7 @@ import com.mercado.paulistinha.service.AuditLogService;
 import com.mercado.paulistinha.service.ProdutoService;
 
 
-
+@CrossOrigin(origins="*")
 @RestController
 @RequestMapping("/produtos")
 public class ProdutoController {
